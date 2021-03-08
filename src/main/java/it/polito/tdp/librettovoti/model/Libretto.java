92872b1f784f6d045cf5aa0e874a30a694252871
nagab <nagab@LAPTOP-MT1TCD0G.lan>
2021-03-08 11:20:07 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -17,6 +17,46 @@ public class Libretto {
 		this.voti.add(v);
 	}
 	
+	/*public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali(int punteggio) { //piu' generale, e' meglio!
+		//calcola una stringa che contiene i voti
+		//sara poi il chiamante a stamparli
+		//Solo il nome?
+		//e il voto? o la data? tutto il voto.toString()
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio){ //ancora meglio del precedente perche' stampa i voti
+		//restituisce solo i voti uguali al criterio
+		//espone all'esterno la struttura dati utilizzata all'interno
+	
+		List<Voto> risultato=new ArrayList<>();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		
+		return risultato;
+	}
+	
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato=new Libretto();
+		for(Voto v:this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+				//risultato.voti.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	
 	public String toString() {
 		//return this.voti.toString(); //chiede alla classe voto di stamparsi, ma anche qui indirizzi di memoria
 		String s="";
